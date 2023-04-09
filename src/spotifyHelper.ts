31299fb029301c1c3ee5072a79ce5968bc2d9f05
Guillaume Munsch <guillaume.munsch@ovrsea.com>
2023-04-09 11:41:50 +0200

    First commit

---
+++ src/spotifyHelper.ts
@@ -0,0 +1,26 @@
+import SpotifyWebApi from 'spotify-web-api-node';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const spotifyHelperInitializer = async () => {
+  const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  });
+  const credentials = await spotifyApi.clientCredentialsGrant();
+  if (!credentials.body?.access_token) {
+    console.log(
+      'Something went wrong when retrieving an access token',
+      credentials,
+    );
+    return null;
+  }
+  console.log('spotifyApi initialized');
+
+  spotifyApi.setAccessToken(credentials.body['access_token']);
+  return spotifyApi;
+};
+
+export class SpotifyHelper {
+  constructor(private spotifyApi: SpotifyWebApi) {}
+}
